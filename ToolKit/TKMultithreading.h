980681d8d6d9bad5e0f4a7a6d200dbba2555ba3d
Zsolt Mikola <zsolt.mikola@westwing.de>
2014-10-03 20:20:11 +0200

    Multithreading is now instance based instead of class based

--- ToolKit/TKMultithreading.h
+++ ToolKit/TKMultithreading.h
@@ -6,13 +6,13 @@
 
 @interface NSObject (TKMultithreading)
 
-+ (void)dispatchBlock:(void(^)(void))block;
-+ (void)dispatchBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue;
-+ (void)dispatchBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue asBarrier:(BOOL)isBarrier;
+- (void)dispatchBlock:(void(^)(void))block;
+- (void)dispatchBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue;
+- (void)dispatchBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue asBarrier:(BOOL)isBarrier;
 
-+ (void)runBlock:(void(^)(void))block;
-+ (void)runBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue;
+- (void)runBlock:(void(^)(void))block;
+- (void)runBlock:(void(^)(void))block onQueue:(dispatch_queue_t)queue;
 
-+ (void)objectForBlock:(void(^)(id))block;
+- (void)objectForBlock:(void(^)(id))block;
 
 @end
--- ToolKit/TKMultithreading.m
+++ ToolKit/TKMultithreading.m
@@ -9,15 +9,15 @@
 // -----------------------------------------------------------------------------
 @implementation NSObject (TKMultithreading)
 
-+ (void)dispatchBlock:(void (^)(void))block{
+- (void)dispatchBlock:(void (^)(void))block{
     [NSObject dispatchBlock:block onQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
 }
 
-+ (void)dispatchBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue{
+- (void)dispatchBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue{
     [NSObject dispatchBlock:block onQueue:queue asBarrier:NO];
 }
 
-+ (void)dispatchBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue asBarrier:(BOOL)isBarrier{
+- (void)dispatchBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue asBarrier:(BOOL)isBarrier{
     if (isBarrier) {
         dispatch_barrier_async(queue, block);
     }else{
@@ -25,17 +25,17 @@
     }
 }
 
-+ (void)runBlock:(void (^)(void))block{
+- (void)runBlock:(void (^)(void))block{
     [NSObject runBlock:block onQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
 }
 
-+(void)runBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue{
+-(void)runBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue{
     dispatch_sync(queue, block);
 }
 
 // -----------------------------------------------------------------------------
 
-+  (void)objectForBlock:(void(^)(id))block{
+-  (void)objectForBlock:(void(^)(id))block{
 	   
 	[NSObject dispatchBlock:^{
 		block([[self class] new]);
@@ -51,17 +51,17 @@
 // -----------------------------------------------------------------------------
 @implementation UIView (TKMultithreading)
 
-+ (void)dispatchBlock:(void(^)(void))block{
+- (void)dispatchBlock:(void(^)(void))block{
     [NSObject dispatchBlock:block onQueue:dispatch_get_main_queue()];
 }
 
-+ (void)runBlock:(void (^)(void))block{
+- (void)runBlock:(void (^)(void))block{
     dispatch_sync(dispatch_get_main_queue(), block);
 }
 
 // -----------------------------------------------------------------------------
 
-+  (void)objectForBlock:(void(^)(id))block{
+-  (void)objectForBlock:(void(^)(id))block{
 	   
 	[UIView dispatchBlock:^{
 		block([[self class] new]);
